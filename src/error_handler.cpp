a7912c0cf96479731c7eed8dfb9570deedbc97a5
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-09-26 23:08:32 +0200

    [ERROR] implement handler_component and handler. Fix volumen_exception class

--- src/error_handler.cpp
+++ src/error_handler.cpp
@@ -1,9 +1,35 @@
 #include "error_handler.hpp"
+#include "utils.hpp"
 
 error::volumen_exception::volumen_exception(const char* msg, const char* FUNCTION_) : 
     std::runtime_error(msg),
+    message(msg),
     FUNCTION(FUNCTION_)
 {}
 
-const char* error::volumen_exception::get_func() const { return FUNCTION; }
+const char* error::volumen_exception::get_func() const noexcept { return FUNCTION; }
 
+const char* error::volumen_exception::what() const noexcept { return message; }
+
+ft::Component error::get_component() { return error_component; }
+
+ft::Component error::handler_component(const char* FUNCTION, const char* what) {
+    auto ok_button = ft::Button("OK", [&]{ show = true; }, utils::button_rounded());
+
+    return ft::Renderer(ok_button, [=]{
+        return ft::vbox({
+            ft::text("An exception occured!"),
+            ft::separator(),
+            ft::text("In:" + std::string(FUNCTION) + ":" + std::string(what))
+            | ft::color(ft::Color::Red),
+            ok_button->Render()
+        }) | ft::borderStyled(ft::Color::Green);
+    });
+}
+
+void error::handler(error::volumen_exception& e) {
+    error_component->Add(handler_component(e.get_func(), e.what()));
+    show = true;
+}
+
+bool* error::get_show() { return &show; }
--- src/error_handler.hpp
+++ src/error_handler.hpp
@@ -1,17 +1,25 @@
+#pragma once
 #include <ftxui/component/component.hpp>
 
 namespace ft = ftxui;
 
 class error {
+    bool show;
+    ft::Component error_component = ft::Container::Vertical({ft::Button("OK", []{})});
+
 public:
     class volumen_exception : std::runtime_error {
+        const char* message;
         const char* FUNCTION;
     public:
         volumen_exception(const char* msg, const char* FUNCTION);
-        const char* get_func() const;
+        const char* get_func() const noexcept;
+        virtual const char* what() const noexcept override;
     };
 
-    ft::Component handler_component();
+    ft::Component handler_component(const char* FUNCTION, const char* what);
+    ft::Component get_component();
+    bool* get_show();
     void handler(volumen_exception& e);
     void handler(std::exception& e);
 };
