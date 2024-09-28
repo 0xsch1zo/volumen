fd637adb08d3a5319c26c94765aed304fd929342
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-09-29 00:18:10 +0200

    [ERROR] imporve error handler. Add a wrapper that catches all necessary exceptions to avoid repeating

--- src/error_handler.cpp
+++ src/error_handler.cpp
@@ -1,26 +1,42 @@
 #include "error_handler.hpp"
 #include "utils.hpp"
 
-error::volumen_exception::volumen_exception(const char* msg, const char* FUNCTION_) : 
-    std::runtime_error(msg),
+error::volumen_exception::volumen_exception(const std::string& msg, const std::string& FUNCTION_) : 
+    std::runtime_error(msg.c_str()),
     message(msg),
     FUNCTION(FUNCTION_)
 {}
 
-const char* error::volumen_exception::get_func() const noexcept { return FUNCTION; }
+const char* error::volumen_exception::get_func() const noexcept { return FUNCTION.c_str(); }
 
-const char* error::volumen_exception::what() const noexcept { return message; }
+const char* error::volumen_exception::what() const noexcept { return message.c_str(); }
 
 ft::Component error::get_component() { return error_component; }
 
-ft::Component error::handler_component(const char* FUNCTION, const char* what) {
-    auto ok_button = ft::Button("OK", [&]{ show = true; }, utils::button_rounded());
+ft::Component error::handler_component(const std::string& FUNCTION, const std::string& what) {
+    auto ok_button = ft::Button("OK", [&]{ show = false; }, utils::button_rounded());
+    const auto error_msg_size = ft::size(ft::WIDTH, ft::EQUAL, 30);
 
     return ft::Renderer(ok_button, [=]{
         return ft::vbox({
             ft::text("An exception occured!"),
             ft::separator(),
-            ft::text("In:" + std::string(FUNCTION) + ":" + std::string(what))
+            ft::paragraph("In: " + FUNCTION + ": " + what)
+            | error_msg_size
+            | ft::color(ft::Color::Red),
+            ok_button->Render()
+        }) | ft::borderStyled(ft::Color::Green);
+    });
+}
+
+ft::Component error::handler_component(const std::string& what) {
+    auto ok_button = ft::Button("OK", [&]{ show = false; }, utils::button_rounded());
+
+    return ft::Renderer(ok_button, [=]{
+        return ft::vbox({
+            ft::text("An exception occured!"),
+            ft::separator(),
+            ft::text(what)
             | ft::color(ft::Color::Red),
             ok_button->Render()
         }) | ft::borderStyled(ft::Color::Green);
@@ -28,8 +44,27 @@ ft::Component error::handler_component(const char* FUNCTION, const char* what) {
 }
 
 void error::handler(error::volumen_exception& e) {
+    error_component->DetachAllChildren();
     error_component->Add(handler_component(e.get_func(), e.what()));
     show = true;
 }
 
+void error::handler(std::exception& e) {
+    error_component->DetachAllChildren();
+    error_component->Add(handler_component(e.what()));
+    show = true;
+}
+
 bool* error::get_show() { return &show; }
+
+void error::wrapper(std::function<void()> caller_lamda) {
+    try{
+        caller_lamda();
+    }
+    catch(error::volumen_exception& ex) {
+        handler(ex);
+    }
+    catch(std::exception& ex) {
+        handler(ex);
+    }
+}
--- src/error_handler.hpp
+++ src/error_handler.hpp
@@ -4,22 +4,24 @@
 namespace ft = ftxui;
 
 class error {
-    bool show;
-    ft::Component error_component = ft::Container::Vertical({ft::Button("OK", []{})});
+    bool show{};
+    ft::Component error_component = ft::Container::Vertical({});
 
 public:
     class volumen_exception : std::runtime_error {
-        const char* message;
-        const char* FUNCTION;
+        const std::string message;
+        const std::string FUNCTION;
     public:
-        volumen_exception(const char* msg, const char* FUNCTION);
+        volumen_exception(const std::string& msg, const std::string& FUNCTION);
         const char* get_func() const noexcept;
         virtual const char* what() const noexcept override;
     };
 
-    ft::Component handler_component(const char* FUNCTION, const char* what);
+    ft::Component handler_component(const std::string& FUNCTION, const std::string& what);
+    ft::Component handler_component(const std::string& what);
     ft::Component get_component();
     bool* get_show();
     void handler(volumen_exception& e);
     void handler(std::exception& e);
+    void wrapper(std::function<void()> caller_lambda);
 };
