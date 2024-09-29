5b0f764ca6181f4d92e2e7d3054c53fb30e333b7
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-09-29 13:33:27 +0200

    [UTILS] add custom_component_window and custom_dropdown

--- src/utils.cpp
+++ src/utils.cpp
@@ -51,17 +51,98 @@ int utils::get_day_of_week(std::string&& date_unformated) {
     else
         return date_out->tm_wday - 1;
 }
+
 // This provides a button with centered text and rounded corners, yeah ...
 ft::ButtonOption utils::button_rounded() {
     auto option = ft::ButtonOption::Border();
     option.transform = [](const ft::EntryState& s) {
         auto element = ft::text(s.label) | ft::center | ft::borderRounded | ft::size(ft::WIDTH, ft::EQUAL, 40);
-        if (s.focused) {
-            element |= ft::bold;
-            element |= ft::bgcolor(ft::Color::White);
-            element |= ft::color(ft::Color::Black);
-        }
+        if (s.focused)
+            return element 
+            | ft::bold
+            | ft::color(ft::Color::Green);
+
+        if (s.active)
+            return element 
+            | ft::bold
+            | ft::color(ft::Color::Red);
+
         return element;
     };
     return option;
+}
+
+ft::Component utils::custom_component_window(ft::Element title, ft::Component contents) {
+    return ft::Renderer(contents, [=]{
+        if(contents->Focused())
+            return ft::window(title, contents->Render() | ft::color(ft::Color::White)) 
+            | ft::color(ft::Color::Green);
+
+        return ft::window(title, contents->Render());
+    });
+}
+
+ft::InputOption utils::plain_input() {
+    return {
+        .transform = [](ft::InputState s){
+            return s.element;
+        }
+    };
+}
+
+ft::Component utils::custom_dropdown(ft::ConstStringListRef entries, int* selected) {
+    ft::Component handle = ft::Dropdown({
+        .checkbox = {
+            .transform = [](const ft::EntryState& s) {
+                auto prefix = ft::text(s.state ? "↓ " : "→ ");  // NOLINT
+                auto t = ft::text(s.label);
+                if ((s.focused || s.active) && !s.state)
+                    t |= ft::bold;
+                return ft::hbox({prefix, t});
+            }
+        },
+        .radiobox = {
+            .entries = entries,
+            .selected = selected,
+            .transform = [](const ft::EntryState& s) {
+#if defined(FTXUI_MICROSOFT_TERMINAL_FALLBACK)
+                // Microsoft terminal do not use fonts able to render properly the default
+                // radiobox glyph.
+                auto prefix = ft::text(s.state ? "(*) " : "( ) ");  // NOLINT
+#else
+                auto prefix = ft::text(s.state ? "◉ " : "○ ");  // NOLINT
+#endif
+                auto e = ft::text(s.label);
+                if (s.active || s.focused)
+                    e |= ft::bold;
+
+                return ft::hbox({prefix, e});
+            }
+        },
+        .transform = [&](
+            bool open, 
+            ft::Element checkbox_element, 
+            ft::Element radiobox_element
+        ) {
+            if (open) {
+                const int max_height = 12;
+                auto menu = ft::vbox({
+                    std::move(checkbox_element),
+                    ft::separator(),
+                    std::move(radiobox_element) 
+                    | ft::vscroll_indicator 
+                    | ft::frame 
+                    | ft::size(ft::HEIGHT, ft::LESS_THAN, max_height),
+                });
+                menu |= handle->Focused() ? ft::borderStyled(ft::Color::Green) : ft::border;
+
+                return menu;
+            }
+            auto menu = ft::vbox({ std::move(checkbox_element), ft::filler() });
+            menu |= handle->Focused() ? ft::borderStyled(ft::Color::Green) : ft::border;
+            return menu;
+        }
+    });
+
+    return handle;
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -12,4 +12,7 @@ public:
     static int get_day_of_week(std::string&& date_unformated);
     static void write_func_cleanup(curlpp::Easy& request);
     static ft::ButtonOption button_rounded();
+    static ft::Component custom_component_window(ft::Element title, ft::Component contents);
+    static ft::InputOption plain_input();
+    static ft::Component custom_dropdown(ft::ConstStringListRef entries, int* selected);
 };
