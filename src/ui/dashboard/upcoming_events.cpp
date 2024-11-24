078602185148499cf7d269ba24b769e28b4bcfa4
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-11-24 18:08:35 +0100

    [UI] Display the nearest events in upcoming_events

--- src/ui/dashboard/upcoming_events.cpp
+++ src/ui/dashboard/upcoming_events.cpp
@@ -3,7 +3,20 @@
 
 ft::Component dashboard::upcoming_events::get_upcoming_events(api* api) {
     auto events = api->get_events_unstructured();
+    const std::string today = api->get_today();
     const int max_event_count = 4;
+
+    // Delete the event if it's date is set to before today
+    std::erase_if(events, [&](const api::event_t& event) {
+        return event.date <= today;
+    });
+
+    // Sort to get the upcoming events first
+    std::sort(events.begin(), events.end(),
+        [](const api::event_t& event_a, const api::event_t& event_b) { 
+            return event_a.date <= event_b.date; 
+        }
+    );
 
     auto events_component = ft::Container::Vertical({});
     int i{};
