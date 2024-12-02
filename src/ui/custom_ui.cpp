3bc4df20831672c944685311e6f48496b344cf1b
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-12-02 20:59:34 +0100

    [UI] Remove custom impl of window because a 58ff448 fixed a bug that cuased window borders to colored improperly

--- src/ui/custom_ui.cpp
+++ src/ui/custom_ui.cpp
@@ -18,106 +18,6 @@ void custom_ui::init(const config* config) {
     config_p = config;
 }
 
-ft::Element custom_ui::custom_window(ft::Element title, ft::Element contents) {
-    using namespace ftxui;
-    class custom_border : public Node {
-    public:
-    custom_border(Elements children,
-          BorderStyle style,
-          std::optional<Color> foreground_color = std::nullopt)
-       : Node(std::move(children)),
-         charset_(simple_border_charset[style])  // NOLINT
-         ,
-         foreground_color_(foreground_color) {}  // NOLINT
-  
-    const Charset& charset_;  // NOLINT
-    std::optional<Color> foreground_color_;
-  
-    void ComputeRequirement() override {
-        Node::ComputeRequirement();
-        requirement_ = children_[0]->requirement();
-        requirement_.min_x += 2;
-        requirement_.min_y += 2;
-        if (children_.size() == 2) {
-          requirement_.min_x =
-              std::max(requirement_.min_x, children_[1]->requirement().min_x + 2);
-        }
-        requirement_.selected_box.x_min++;
-        requirement_.selected_box.x_max++;
-        requirement_.selected_box.y_min++;
-        requirement_.selected_box.y_max++;
-   }
-  
-   void SetBox(Box box) override {
-        Node::SetBox(box);
-        if (children_.size() == 2) {
-          Box title_box;
-          title_box.x_min = box.x_min + 1;
-          title_box.x_max = box.x_max - 1;
-          title_box.y_min = box.y_min;
-          title_box.y_max = box.y_min;
-          children_[1]->SetBox(title_box);
-        }
-        box.x_min++;
-        box.x_max--;
-        box.y_min++;
-        box.y_max--;
-        children_[0]->SetBox(box);
-   }
-  
-   void Render(Screen& screen) override {
-        // Draw content.
-        children_[0]->Render(screen);
-    
-        // Draw the border.
-        if (box_.x_min >= box_.x_max || box_.y_min >= box_.y_max) {
-          return;
-        }
-    
-        screen.at(box_.x_min, box_.y_min) = charset_[0];  // NOLINT
-        screen.at(box_.x_max, box_.y_min) = charset_[1];  // NOLINT
-        screen.at(box_.x_min, box_.y_max) = charset_[2];  // NOLINT
-        screen.at(box_.x_max, box_.y_max) = charset_[3];  // NOLINT
-    
-        for (int x = box_.x_min + 1; x < box_.x_max; ++x) {
-          Pixel& p1 = screen.PixelAt(x, box_.y_min);
-          Pixel& p2 = screen.PixelAt(x, box_.y_max);
-          p1.character = charset_[4];  // NOLINT
-          p2.character = charset_[4];  // NOLINT
-          p1.automerge = true;
-          p2.automerge = true;
-        }
-
-        for (int y = box_.y_min + 1; y < box_.y_max; ++y) {
-          Pixel& p3 = screen.PixelAt(box_.x_min, y);
-          Pixel& p4 = screen.PixelAt(box_.x_max, y);
-          p3.character = charset_[5];  // NOLINT
-          p4.character = charset_[5];  // NOLINT
-          p3.automerge = true;
-          p4.automerge = true;
-        }
-
-        // Draw the border color.
-        if (foreground_color_) {
-          for (int x = box_.x_min; x <= box_.x_max; ++x) {
-            screen.PixelAt(x, box_.y_min).foreground_color = *foreground_color_;
-            screen.PixelAt(x, box_.y_max).foreground_color = *foreground_color_;
-          }
-          for (int y = box_.y_min; y <= box_.y_max; ++y) {
-            screen.PixelAt(box_.x_min, y).foreground_color = *foreground_color_;
-            screen.PixelAt(box_.x_max, y).foreground_color = *foreground_color_;
-          }
-        }
-
-        // Draw title.
-        if (children_.size() == 2) {
-          children_[1]->Render(screen);
-        }
-    }
- };
-    return std::make_shared<custom_border>(unpack(std::move(contents), std::move(title | ft::notflex)), ROUNDED);
-}
-
 // This provides a button with centered text and rounded corners, yeah ...
 ft::ButtonOption custom_ui::button_rounded()
 {
@@ -139,8 +39,8 @@ ft::ButtonOption custom_ui::button_rounded()
 ft::Element custom_ui::focus_managed_window(ft::Element title, ft::Element contents, const focus_management_t& focus_management)
 {
     auto base = [&](ft::Color title_color, ft::Color overall_color){
-        return custom_window(
-            ft::hbox({ ft::hbox(title| ft::color(title_color)) | ft::flex}) | ft::flex,
+        return ft::window(
+            title | ft::color(title_color),
             contents | ft::color(ft::Color::White)
         ) | ft::color(overall_color); 
     };
